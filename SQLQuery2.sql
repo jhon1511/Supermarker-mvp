f99b87e0d5ce548bef4e5af5b5de2c5fcb09a8c7
Usuario1 <Usuario1@DESKTOP-LAS9AIG>
2023-10-16 22:08:40 -0500

    Agregar archivos de proyecto.

---
+++ SQLQuery2.sql
@@ -0,0 +1,12 @@
+﻿USE Supermarker; 
+
+CREATE TABLE PayMode
+(
+    Pay_Mode_Id INT IDENTITY(100000, 1) PRIMARY KEY, 
+    Pay_Mode_Name NVARCHAR(50) NOT NULL,
+    Pay_Mode_Observation NVARCHAR(50) NOT NULL
+); 
+
+INSERT INTO PayMode (Pay_Mode_Name, Pay_Mode_Observation) VALUES
+('Cash', 'Cash mode'),
+('Credit card', 'Credit card mode');
